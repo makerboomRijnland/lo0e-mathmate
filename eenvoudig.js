4b0322158dcce8a9344b8d5c79b1d7820a4ea493
Marcel Akerboom @ mboRijnland <makerboom@mborijnland.nl>
2021-05-25 13:10:44 +0200

    Vermenigvuldigtal geimplementeerd

--- eenvoudig.js
+++ eenvoudig.js
@@ -1,6 +1,10 @@
 function genereerTafel() {
+    var vermenigvuldigtal = document.getElementById("tafel-vermenigvuldigtal").value; // De invoer van de gebruiker
     var antwoordVeld = document.getElementById("tafel-antwoord");
-    antwoordVeld.value = "1 x 1 = 1";
-    
+
+    var product = 1 * vermenigvuldigtal;  // We rekenen het product uit van 1 vermenigvuldiging
+    var vermenigvuldiging = "1 x " + vermenigvuldigtal + " = " + product; // Dit is de tekstuele berekening die we kunnen toevoegen aan het antwoordveld.
+    antwoordVeld.value = vermenigvuldiging;
+
     return false; // We moeten false returnen, om te voorkomen dat de browser de pagina herlaad.
 }
